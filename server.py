bc80c889ac98f4286427c4a67b1ad4034eada57c
tylerhoffs <tyler.hoffman@students.makeschool.com>
2015-10-16 11:01:21 -0700

    Added some authorisation checks

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from utils.mongo_json_encoder import JSONEncoder
+from functools import wraps
 
 # Basic Setup
 app = Flask(__name__)
@@ -10,31 +11,119 @@ mongo = MongoClient('localhost', 27017)
 app.db = mongo.develop_database
 api = Api(app)
 
-#Implement REST Resource
+
+def check_auth(username, password):
+    return username == 'admin' and password == 'secret'
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            message = {'error': 'Basic Auth Required.'}
+            resp = jsonify(message)
+            resp.status_code = 401
+            return resp
+
+        return f(*args, **kwargs)
+    return decorated
+
+
+# Implement REST Resource
 class MyObject(Resource):
 
     def post(self):
-      new_myobject = request.json
-      myobject_collection = app.db.myobjects
-      result = myobject_collection.insert_one(request.json)
+        new_myobject = request.json
+        myobject_collection = app.db.myobjects
+        result = myobject_collection.insert_one(request.json)
 
-      myobject = myobject_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        myobject = myobject_collection.find_one({"_id": ObjectId(result.inserted_id)})
 
-      return myobject
-
-    def get(self, myobject_id):
-      myobject_collection = app.db.myobjects
-      myobject = myobject_collection.find_one({"_id": ObjectId(myobject_id)})
-
-      if myobject is None:
-        response = jsonify(data=[])
-        response.status_code = 404
-        return response
-      else:
         return myobject
 
+    def get(self, myobject_id):
+        myobject_collection = app.db.myobjects
+        myobject = myobject_collection.find_one({"_id": ObjectId(myobject_id)})
+
+        if myobject is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            return myobject
+
 # Add REST resource to API
-api.add_resource(MyObject, '/myobject/','/myobject/<string:myobject_id>')
+api.add_resource(MyObject, '/myobject/', '/myobject/<string:myobject_id>')
+
+
+class User(Resource):
+    def post(self):
+        new_user = request.json
+        user_collection = app.db.users
+        result = user_collection.insert_one(request.json)
+
+        user = user_collection.find_one({"_id": ObjectId(result.inserted_id)})
+
+        return user
+
+    def get(self, user_id):
+        user_collection = app.db.users
+        user = user_collection.find_one({"_id": ObjectId(user_id)})
+
+        if user is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            return user
+
+api.add_resource(User, '/users/', '/users/<string:user_id>')
+
+
+class Trip(Resource):
+    @requires_auth
+    def post(self):
+        new_trip = request.json
+        trip_collection = app.db.trips
+        result = trip_collection.insert_one(request.json)
+
+        trip = trip_collection.find_one({"_id": ObjectId(result.inserted_id)})
+
+        return trip
+
+    @requires_auth
+    def get(self, trip_id):
+        trip_collection = app.db.trips
+        trip = trip_collection.find_one({"_id": ObjectId(trip_id)})
+
+        if trip is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            return trip
+
+    def delete(self, trip_id):
+        new_trip = request.json
+        trip_collection = app.db.trips
+        result = trip_collection.insert_one(request.json)
+
+        trip = trip_collection.find_one({"_id": ObjectId(result.inserted_id)})
+
+        return trip
+
+    def delete(self, trip_id):
+        new_trip = request.json
+        trip_collection = app.db.trips
+        result = trip_collection.insert_one(request.json)
+
+        trip = trip_collection.find_one({"_id": ObjectId(result.inserted_id)})
+
+        return trip
+
+api.add_resource(Trip, '/trips/', '/trips/<string:trip_id>')
+
 
 # provide a custom JSON serializer for flaks_restful
 @api.representation('application/json')
